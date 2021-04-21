db77accb6d1d2056d10274e05a1f295212e1ae5d
Kyler <47895580+rkylerd@users.noreply.github.com>
2021-04-21 14:13:40 -0600

    Added tests for the Navigation component.

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ module.exports = {
     // Jest transformations -- this adds support for TypeScript
     // using ts-jest
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.svg$": "<rootDir>/src/__tests__/jest/svgTransform.js"
     },
 
     // Runs special logic, such as cleaning up components
@@ -21,7 +22,7 @@ module.exports = {
     // Test spec file resolution pattern
     // Matches parent folder `__tests__` and filename
     // should contain `test` or `spec`.
-    testRegex: "(/src/__tests__/(pages|components)/.*|(\\.|/)(test|spec))\\.tsx?$",
+    testRegex: "(/src/__tests__/(pages|components|layouts)/.*|(\\.|/)(test|spec))\\.tsx?$",
 
     // Module file extensions for importing
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
